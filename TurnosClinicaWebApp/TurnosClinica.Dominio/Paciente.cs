3d4932a1cdc169c92200b3fdcc5c58c4a8c517d5
AlmadaMauricio <110863446+AlmadaMauricio@users.noreply.github.com>
2026-05-31 18:20:16 -0300

    creando la clase Paciente

---
+++ TurnosClinicaWebApp/TurnosClinica.Dominio/Paciente.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TurnosClinica.Dominio
+{
+    public class Paciente
+    {
+        public int IdPaciente { get; set; }
+        public string Dni { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public string Telefono { get; set; }
+        public string Email { get; set; }
+        public string Direccion { get; set; }
+        public bool Activo { get; set; } = true;
+    }
+}
