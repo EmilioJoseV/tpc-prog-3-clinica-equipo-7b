75ef8084011bc78cf8844a83bb8880f81d37a2ec
Cyntia-000 <cyntianoemirodriguez>
2026-06-01 00:54:56 -0300

    creacion de clase usuario

---
+++ TurnosClinicaWebApp/TurnosClinica.Dominio/Usuario.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TurnosClinica.Dominio
+{
+    public class Usuario
+    {
+        public int IdUsuario{ get; set; }
+        public string NombreUsuario { get; set; }
+
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+        public int IdRol { get; set; }
+        public int ? IdMedico { get; set; }
+        public bool Activo { get; set; }
+        
+    }
+}
