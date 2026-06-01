93708c1a8a4abe8a9e0e921d913bf575d6d26ef0
Cyntia-000 <cyntianoemirodriguez>
2026-05-31 21:28:10 -0300

    agrego la clase MedicoEspecialidad

---
+++ TurnosClinicaWebApp/TurnosClinica.Dominio/MedicoEspecialidad.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TurnosClinica.Dominio
+{
+    internal class MedicoEspecialidad
+    {
+        public int IdMedico { get; set; }
+        public int IdEspecialidad { get; set; }
+
+    }
+}
