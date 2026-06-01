b8b1eab2f2d526757778855d50f0ad4bd4d6fe46
Cyntia-000 <cyntianoemirodriguez>
2026-05-31 21:41:21 -0300

    creacion de la clase DiaAtencionMedico

---
+++ TurnosClinicaWebApp/TurnosClinica.Dominio/DiaAtencionMedico.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TurnosClinica.Dominio
+{
+    internal class DiaAtencionMedico
+    {
+        public int IdDiaAtencionMedico { get; set; }
+        public int IdMedico { get; set; }
+        public byte DiaSemana { get; set; }
+        public TimeSpan HoraDesde { get; set; }
+        public TimeSpan HoraHasta { get; set; }
+        public bool Activo { get; set; }
+    }
+}
