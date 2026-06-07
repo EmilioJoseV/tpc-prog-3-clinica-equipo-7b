d1024a8cf09a51271ef4a1f4d7944f5b5776ec15
Emilio Vera <emiliojoseveraparra@gmail.com>
2026-06-07 14:46:00 -0300

    Agregar firmas de metodos para capa de acceso datos

--- TurnosClinicaWebApp/TurnosClinica.AccesoDatos/PacienteDatos.cs
+++ TurnosClinicaWebApp/TurnosClinica.AccesoDatos/PacienteDatos.cs
@@ -1,27 +1,124 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using TurnosClinica.Dominio.Entidades;
 
 namespace TurnosClinica.AccesoDatos
 {
     public class PacienteDatos
     {
-        private readonly AccesoDatos AccesoDatos;
-
+        private readonly AccesoDatos accesoDatos;
 
         public PacienteDatos()
         {
-            AccesoDatos = new AccesoDatos();
+            accesoDatos = new AccesoDatos();
         }
 
         public List<Paciente> Listar()
         {
+            List<Paciente> pacientes = new List<Paciente>();
             try
             {
-                return new List<Paciente>();
+                return pacientes;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public Paciente ObtenerPorId(int idPaciente)
+        {
+            Paciente paciente = new Paciente();
+            try
+            {
+                return paciente;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public List<Paciente> ObtenerTodosActivos()
+        {
+            List<Paciente> pacientes = new List<Paciente>();
+            try
+            {
+                return pacientes;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public Paciente ObtenerPorDni(string dni)
+        {
+            Paciente paciente = new Paciente();
+            try
+            {
+                return paciente;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public Paciente ObtenerPorEmail(string email)
+        {
+            Paciente paciente = new Paciente();
+            try
+            {
+                return paciente;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public bool ExisteDni(string dni)
+        {
+            try
+            {
+                return false;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public bool ExisteEmail(string email)
+        {
+            try
+            {
+                return false;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public int Agregar(Paciente paciente)
+        {
+            try
+            {
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public bool Modificar(Paciente paciente)
+        {
+            try
+            {
+                return false;
             }
             catch (Exception ex)
             {
