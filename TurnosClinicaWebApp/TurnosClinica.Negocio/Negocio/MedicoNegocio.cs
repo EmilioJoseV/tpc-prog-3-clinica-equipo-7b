6a8da0c1731e0b26bdef1bced02bade5214be82c
Emilio Vera <emiliojoseveraparra@gmail.com>
2026-06-08 21:51:42 -0300

    agregr primeras clases de negocio

--- TurnosClinicaWebApp/TurnosClinica.Negocio/Negocio/MedicoNegocio.cs
+++ TurnosClinicaWebApp/TurnosClinica.Negocio/Negocio/MedicoNegocio.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TurnosClinica.Negocio
+namespace TurnosClinica.Negocio.Negocio
 {
-    public class Class1
+    internal class MedicoNegocio
     {
     }
 }
---
+++ TurnosClinicaWebApp/TurnosClinica.Negocio/Service/EmailService.cs
@@ -0,0 +1,6 @@
+﻿namespace TurnosClinica.Negocio
+{
+    internal class EmailService
+    {
+    }
+}
