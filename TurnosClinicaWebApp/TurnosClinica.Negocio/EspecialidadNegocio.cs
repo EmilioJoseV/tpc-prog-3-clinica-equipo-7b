11c8eaf418ba9dc5a7b0b8846d3d366dad2ddfeb
Emilio Vera <emiliojoseveraparra@gmail.com>
2026-06-11 01:06:57 -0300

    ajustar modificadores de acceso clases negocio

--- TurnosClinicaWebApp/TurnosClinica.Negocio/EspecialidadNegocio.cs
+++ TurnosClinicaWebApp/TurnosClinica.Negocio/EspecialidadNegocio.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace TurnosClinica.Negocio
 {
-    internal class EspecialidadNegocio
+    public class EspecialidadNegocio
     {
     }
 }
--- TurnosClinicaWebApp/TurnosClinica.Negocio/UsuarioNegocio.cs
+++ TurnosClinicaWebApp/TurnosClinica.Negocio/UsuarioNegocio.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace TurnosClinica.Negocio
 {
-    internal class UsuarioNegocio
+    public class UsuarioNegocio
     {
     }
 }
