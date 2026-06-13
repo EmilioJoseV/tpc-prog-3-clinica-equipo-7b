ca70b2a9d6df6e885e5a065235fa76765f78d52a
Cyntia-000 <cyntianoemirodriguez>
2026-06-13 20:34:13 -0300

    propiedad para enviar la lista,traer especialidades activas,configuracion del page-load

--- TurnosClinicaWebApp/TurnosClinica.Web/Especialidades/ListaEspecialidades.aspx.cs
+++ TurnosClinicaWebApp/TurnosClinica.Web/Especialidades/ListaEspecialidades.aspx.cs
@@ -1,12 +1,31 @@
 using System;
-using System.Web.UI;
+using System.Collections.Generic;
+using TurnosClinica.Dominio.Entidades;
+using TurnosClinica.Negocio;
 
 namespace TurnosClinica.Web
 {
-    public partial class ListaEspecialidades : Page
+    public partial class ListaEspecialidades : System.Web.UI.Page
     {
+        
+        public List<Especialidad> ListaEspecialidad { get; set; }
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            EspecialidadNegocio negocio = new EspecialidadNegocio();
+
+            try
+            {
+                if (!IsPostBack)
+                {
+                    
+                    ListaEspecialidad = negocio.Listar(true);
+                }
+            }
+            catch (Exception ex)
+            {
+                Session.Add("error", ex.ToString());
+            }
         }
     }
 }
