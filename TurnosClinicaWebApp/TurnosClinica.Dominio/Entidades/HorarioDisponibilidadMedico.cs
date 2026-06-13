4a06434fbda81182e8a529e71e249d564dae9698
Emilio Vera <emiliojoseveraparra@gmail.com>
2026-06-13 14:38:30 -0300

    Agregar clases de dominio, y enumerador de dias de semana

--- TurnosClinicaWebApp/TurnosClinica.Dominio/Entidades/HorarioDisponibilidadMedico.cs
+++ TurnosClinicaWebApp/TurnosClinica.Dominio/Entidades/HorarioDisponibilidadMedico.cs
@@ -1,11 +1,13 @@
 using System;
+using TurnosClinica.Dominio.Enums;
 
 namespace TurnosClinica.Dominio.Entidades
 {
     public class HorarioDisponibilidadMedico
     {
         public int IdHorarioDisponibilidadMedico { get; set; }
-        public int DiaSemana { get; set; }
+        public int IdMedico { get; set; }
+        public DiaSemanaEnum DiaSemana { get; set; }
         public TimeSpan HoraDesde { get; set; }
         public TimeSpan HoraHasta { get; set; }
         public bool Activo { get; set; } = true;
---
+++ TurnosClinicaWebApp/TurnosClinica.Dominio/Enums/DiaSemanaEnum.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace TurnosClinica.Dominio.Enums
+{
+    public enum DiaSemanaEnum
+    {
+        Lunes = 1,
+        Martes = 2,
+        Miercoles = 3,
+        Jueves = 4,
+        Viernes = 5,
+        Sabado = 6,
+        Domingo = 7
+    }
+}
