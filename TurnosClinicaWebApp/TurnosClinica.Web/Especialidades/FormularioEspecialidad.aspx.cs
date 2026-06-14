0f5920e785bda6e0adbb27da36029cc685157063
Cyntia-000 <cyntianoemirodriguez>
2026-06-14 01:09:18 -0300

    pantalla error con mensaje y uso del bootstrap para que el error este en un recuadro rojo

--- TurnosClinicaWebApp/TurnosClinica.Web/Especialidades/FormularioEspecialidad.aspx.cs
+++ TurnosClinicaWebApp/TurnosClinica.Web/Especialidades/FormularioEspecialidad.aspx.cs
@@ -10,17 +10,28 @@ namespace TurnosClinica.Web
         }
         protected void btnAceptar_Click(object sender, EventArgs e)
         {
-            TurnosClinica.Dominio.Entidades.Especialidad nuevaEspecialidad = new TurnosClinica.Dominio.Entidades.Especialidad();
-            nuevaEspecialidad.Nombre = txtNombre.Text;
-            nuevaEspecialidad.Descripcion = txtDescripcion.Text;
+            try
+            {
+                TurnosClinica.Dominio.Entidades.Especialidad nuevaEspecialidad = new TurnosClinica.Dominio.Entidades.Especialidad();
+                nuevaEspecialidad.Nombre = txtNombre.Text;
+                nuevaEspecialidad.Descripcion = txtDescripcion.Text;
 
-            
-            TurnosClinica.Negocio.EspecialidadNegocio negocio = new TurnosClinica.Negocio.EspecialidadNegocio();
-            negocio.Agregar(nuevaEspecialidad);
-            
-            Response.Redirect("ListaEspecialidades.aspx", false);
+                TurnosClinica.Negocio.EspecialidadNegocio negocio = new TurnosClinica.Negocio.EspecialidadNegocio();
+                negocio.Agregar(nuevaEspecialidad);
+
+                Response.Redirect("ListaEspecialidades.aspx", false);
+            }
+            catch (Exception ex)
+            {
+                
+                Session.Add("error", ex.Message);
+
+                
+                Response.Redirect("../Error.aspx", false);
+            }
 
         }
+
         protected void btnCancelar_Click(object sender, EventArgs e)
         {
             
